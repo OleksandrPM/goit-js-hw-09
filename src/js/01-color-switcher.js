aea62a0b3098e862d94ae954d9f7fb52c7e31bb2
Oleksandr Moskaliuk <olekspm@gmail.com>
2023-03-07 17:03:37 +0100

    Done first task, added flatpickr

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const colorChangingTime = 1000;
 
+const bodyEl = document.querySelector('body');
+
+bodyEl.addEventListener('click', onStartButtonClick);
+
+let intervalId;
+
+function onStartButtonClick(event) {
+  if (event.target.hasAttribute('data-start')) {
+    intervalId = setInterval(() => {
+      bodyEl.style.backgroundColor = getRandomHexColor();
+    }, colorChangingTime);
+    bodyEl.querySelector('[data-start]').disabled = true;
+    bodyEl.querySelector('[data-stop]').disabled = false;
+  }
+
+  if (event.target.hasAttribute('data-stop')) {
+    clearInterval(intervalId);
+    bodyEl.querySelector('[data-start]').disabled = false;
+    bodyEl.querySelector('[data-stop]').disabled = true;
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
